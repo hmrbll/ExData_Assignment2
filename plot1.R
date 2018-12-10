4b84c88785ade7e3ae49e74b7c51d5f2fd6f1edd
hmrbll <huseyinemre_balli@hotmail.com>
2018-12-10 20:10:54 +0300

    Plot 1

---
+++ plot1.R
@@ -0,0 +1,16 @@
+#Set file name
+datafile <- "summarySCC_PM25.rds"
+#Read file
+dataset <- readRDS(datafile)
+#Subset the data according to needs of question
+subdata <- data.frame(Emissions = dataset$Emissions, year = dataset$year)
+#Aggregate total emission by each year
+library(dplyr)
+aggdata <- subdata %>%
+        group_by(year) %>%
+                summarise(sum = sum(Emissions, na.rm = TRUE))
+#Plotting
+png("plot1.png", width = 480, height = 480)
+with(aggdata, plot(year, sum, type = "l"))
+with(aggdata, points(year, sum, col = "red", pch = 19))
+dev.off()
